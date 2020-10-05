76f1575d76386f28bf2728c7e3cd7550f341a93f
Kennedy Idialu <contact@makeos.org>
2020-10-05 13:40:37 +0100

    Brew formula update for lobe version v0.0.11

--- Formula/lobe.rb
+++ Formula/lobe.rb
@@ -2,16 +2,16 @@
 class Lobe < Formula
   desc ""
   homepage "https://makeos.org/"
-  version "0.0.10"
+  version "0.0.11"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/make-os/lobe/releases/download/v0.0.10/lobe_0.0.10_Darwin_x86_64.tar.gz"
-    sha256 "5f4658acc365725d1192ce9d5e6afaf2391a883414dc49bf7d6bddbae286767e"
+    url "https://github.com/make-os/lobe/releases/download/v0.0.11/lobe_0.0.11_Darwin_x86_64.tar.gz"
+    sha256 "e1ecb6733d478c05f808b8b5194ddf08d83bc97e9ca8e8d45ae994e9b7cb64db"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/make-os/lobe/releases/download/v0.0.10/lobe_0.0.10_Linux_x86_64.tar.gz"
-      sha256 "250ca9d06c9a6a270c7b22d8c00f0f0a41ddfb52767ff6520553e3a0a2a5baca"
+      url "https://github.com/make-os/lobe/releases/download/v0.0.11/lobe_0.0.11_Linux_x86_64.tar.gz"
+      sha256 "8e26be4a3a38959e682dd0aa7f228aa3c9839a4e312daa6cc349b8de296fe1f7"
     end
   end
   
