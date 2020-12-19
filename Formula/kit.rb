17cf5f874557df1ac1bfc036a387fbb7597ec178
MakeOS Team <contact@makeos.org>
2020-12-19 15:42:51 +0100

    Brew formula update for kit version v0.0.19

--- Formula/kit.rb
+++ Formula/kit.rb
@@ -2,19 +2,18 @@
 class Kit < Formula
   desc "A client for the MakeOS decentralized code collaboration network"
   homepage "https://makeos.org/"
-  version "0.0.18"
+  version "0.0.19"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/make-os/kit/releases/download/v0.0.18/kit_0.0.18_Darwin_x86_64.tar.gz"
-    sha256 "d0b80a63d0568746bd22907580072f6b3cf19fbc3f7fdd1c1fe81a90df6c7aa6"
-  elsif OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/make-os/kit/releases/download/v0.0.18/kit_0.0.18_Linux_x86_64.tar.gz"
-      sha256 "3552f284a95d9cfe8ce063ffc47dcfddcb5f6b49dafa19ee698a0518618e7b5e"
-    end
+    url "https://github.com/make-os/kit/releases/download/v0.0.19/kit_0.0.19_Darwin_x86_64.tar.gz"
+    sha256 "684a9978e2f8a174a75b2d1b729f15f5848280a808893a867383c8641a8863fc"
   end
-  
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/make-os/kit/releases/download/v0.0.19/kit_0.0.19_Linux_x86_64.tar.gz"
+    sha256 "288f45550ad4709431ef33baf5259d575c60b88e850c2f657fd743851c2b02c8"
+  end
+
   depends_on "git"
 
   def install
